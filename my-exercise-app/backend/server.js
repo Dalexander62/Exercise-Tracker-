8dc3a383d4e1b223b3e79949282fc94ac4cfe6cd
Dan Robinson <danielarobinson2@gmail.com>
2022-02-16 21:14:49 +0000

    backend connected to server

---
+++ my-exercise-app/backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+// require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(
+  "mongodb+srv://Dalex:mongoDB@cluster0.m3ijd.mongodb.net/test",
+  { useNewUrlParser: true },
+  (error) => {
+    if (error) {
+      console.log("Can't connect");
+    } else if (!error) {
+      console.log("Connected");
+    }
+  }
+);
+
+const server = app.listen(5015, () => {
+  console.log(`Server is running on port: ${5015}`);
+});
